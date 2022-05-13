7e76b7e9000a7984bedc15a3431b291780df1cb9
radonnachie <code@radonn.co.za>
2022-05-13 10:02:22 +0000

    ~ Nodes/FPGA/systemd_services casperfpga_rest

--- Nodes/FPGA/systemd_services/install.sh
+++ Nodes/FPGA/systemd_services/install.sh
@@ -5,15 +5,11 @@ then
   echo "Please run with super-user privileges"
   exit 1
 else
-	#cp ./init_casperfpga_rest_server.sh /usr/local/bin/
-	#cp ./casperfpga_rest_server.service /etc/systemd/system/
 
 	cp ./init_remoteobjects_server.sh /usr/local/bin/
 	cp ./remoteobjects_server.service /etc/systemd/system/
 
-	#systemctl disable casperfpga_rest_server
 	systemctl disable remoteobjects_server
 	systemctl daemon-reload
-	#systemctl enable casperfpga_rest_server
 	systemctl enable remoteobjects_server
 fi
