77781ea7b8ceb374b16a046b4c8a8d8c2316e2e5
radonnachie <code@radonn.co.za>
2023-03-22 10:25:30 +0000

    + Nodes/GPU/systemd_services hpdaq_secondary service for instances 2,3 hpguppi_daq/d2157b5

--- Nodes/GPU-Compute/systemd_services/install.sh
+++ Nodes/GPU-Compute/systemd_services/install.sh
@@ -5,10 +5,12 @@ then
   echo "Please run with super-user privileges"
   exit 1
 else
-	cp ./hpdaq.service /etc/systemd/system/
+	cp ./hpdaq*.service /etc/systemd/system/
 	cp ./hpdaq_service.conf /home/cosmic
 
 	systemctl disable hpdaq
+	systemctl disable hpdaq_secondary
 	systemctl daemon-reload
 	systemctl enable hpdaq
+	systemctl enable hpdaq_secondary
 fi
