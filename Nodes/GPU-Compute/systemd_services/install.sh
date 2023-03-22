4daf4cb950ef2e11b4c46f752f5f042c0aaa95ba
radonnachie <code@radonn.co.za>
2023-03-22 08:07:22 +0000

    ** Nodes/GPU-Compute/systemd_services merge redis_gateway into hpdaq service, use --delete in exec stop hpguppi_daq/1357831

--- Nodes/GPU-Compute/systemd_services/install.sh
+++ Nodes/GPU-Compute/systemd_services/install.sh
@@ -5,14 +5,10 @@ then
   echo "Please run with super-user privileges"
   exit 1
 else
-	cp ./hpdaq*.service /etc/systemd/system/
-	cp ./redis_gateway.service /etc/systemd/system/
+	cp ./hpdaq.service /etc/systemd/system/
 	cp ./hpdaq_service.conf /home/cosmic
-	cp ./redis_gateway_service.conf /home/cosmic
 
 	systemctl disable hpdaq
-	systemctl disable redis_gateway
 	systemctl daemon-reload
 	systemctl enable hpdaq
-	systemctl enable redis_gateway
 fi
