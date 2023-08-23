24deca4f6ed8fa5ba1dc3248e8a16465b3673108
talonmyburgh <talon.myburgh@mydon.co.za>
2023-08-23 08:38:14 +0000

    ++ Nodes/GPU/systemd/nvidia_power_limit

--- Nodes/GPU-Compute/systemd_services/install.sh
+++ Nodes/GPU-Compute/systemd_services/install.sh
@@ -5,12 +5,15 @@ then
   echo "Please run with super-user privileges"
   exit 1
 else
+	cp ./nvidia_power_limit.service /etc/systemd/system/
 	cp ./hpdaq*.service /etc/systemd/system/
 	cp ./hpdaq_service.conf /home/cosmic
 
+	systemctl disable nvidia_power_limit
 	systemctl disable hpdaq
 	systemctl disable hpdaq_secondary
 	systemctl daemon-reload
+	systemctl enable nvidia_power_limit
 	systemctl enable hpdaq
 	systemctl enable hpdaq_secondary
 fi
